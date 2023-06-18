e565ce008b3634b55f2c83fa22a4000aee372cd9
ahsan7266 <74853314+ahsan7266@users.noreply.github.com>
2023-06-18 15:39:20 +0500

    done

--- UI/MS_UI/Controllers/StudentController.cs
+++ UI/MS_UI/Controllers/StudentController.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MS_UI.Models.AdmissionModels;
 
 namespace MS_UI.Controllers
 {
     public class StudentController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Admission(AdmissionViewModel model)
         {
             return View();
         }
--- UI/MS_UI/Services/CommoneService.cs
+++ UI/MS_UI/Services/CommoneService.cs
@@ -9,35 +9,35 @@ namespace MS_UI.Services
     {
         public async Task GetCampus()
         {
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    client.BaseAddress = Constrant.AppUrl;
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //try
+            //{
+            //    using (HttpClient client = new HttpClient())
+            //    {
+            //        client.BaseAddress = Constrant.AppUrl;
+            //        client.DefaultRequestHeaders.Accept.Clear();
+            //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                    //Get Method
-                    HttpResponseMessage response = await client.GetAsync("ComplaintPortal/GetFromDepartment?DeptCode=" + DeptCode);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var Data = await response.Content.ReadAsStringAsync();
-                        var res = JsonConvert.DeserializeObject<Response<FromDepartment>>(Data);
-                        if (!res.Status)
-                            ViewBag.Error = res.Message;
+            //        //Get Method
+            //        HttpResponseMessage response = await client.GetAsync("ComplaintPortal/GetFromDepartment?DeptCode=" + DeptCode);
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            var Data = await response.Content.ReadAsStringAsync();
+            //            var res = JsonConvert.DeserializeObject<Response<FromDepartment>>(Data);
+            //            if (!res.Status)
+            //                ViewBag.Error = res.Message;
 
-                        ViewBag.FromDepartment = new SelectList(res.List, "DeptCode", "DeptName");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Internal server Error");
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            //            ViewBag.FromDepartment = new SelectList(res.List, "DeptCode", "DeptName");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Internal server Error");
+            //        }
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
         }
     }
 }
